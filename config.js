3cf4b27ec19a517b7ece74cf797899870075778a
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-01-16 17:41:53 +0530

    Added config file and updated model

---
+++ config.js
@@ -0,0 +1,3 @@
+var dbURL= 'mongodb://navinprakash:123456@ds037415.mongolab.com:37415/mnotes';
+var secret = 'radiantnavin';
+
--- model.js
+++ model.js
@@ -1,8 +1,9 @@
 var mongoose =require('mongoose');
 
 var schema = new mongoose.Schema({
-    
-    
+    name :String,
+    password :String,
+    admin : Boolean
 });
 
 module.exports.userSchema = schema;
