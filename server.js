5f498b795164f04494fec6f60b00dab247aa9209
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-01-16 19:33:52 +0530

    configure some more settings

--- server.js
+++ server.js
@@ -2,12 +2,25 @@ var mongoose =require('mongoose');
 var express = require ('express');
 var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
-
+var morgan = require('morgan');
 var config = require('./config.js');
 var User = require('./model.js');
 
 var app =express();
 var port = 8080;
+
+mongoose.connect(config.dbURL,function(error){
+    if(error){
+        console.log('Attempted DB Connection has failed');
+    }
+     else console.log ('Successfully connected to Database');   
+});
+
+app.set('superSecret',config.secret);
+app.use(bodyParser.urlencoded({extended :false}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
 
 app.get('/sample', function(request,response){
    response.send("hello this is sample");
@@ -16,10 +29,6 @@ app.get('/sample', function(request,response){
 
 app.listen(port);
 console.log('listening on' + port);
+console.log('value of supersecret is' + app.get('superSecret'));
 
-mongoose.connect(config.dbURL,function(error){
-    if(error){
-        console.log('Attempted DB Connection has failed');
-    }
-     else console.log ('Successfully connected to Database');   
-});
+
