c24ce75084838c2c12d4e613ecf30bb5a732ecd1
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-01-16 18:28:04 +0530

    Added config and connected to DB

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 var dbURL= 'mongodb://navinprakash:123456@ds037415.mongolab.com:37415/mnotes';
 var secret = 'radiantnavin';
 
+module.exports.secret = secret;
+module.exports.dbURL =dbURL;
+
--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 var mongoose =require('mongoose');
 var express = require ('express');
+var jwt = require('jsonwebtoken');
+var bodyParser = require('body-parser');
+
+var config = require('./config.js');
+var User = require('./model.js');
+
 var app =express();
 var port = 8080;
 
 app.get('/sample', function(request,response){
-   response.send("hello this is sample")
+   response.send("hello this is sample");
     
 });
 
 app.listen(port);
 console.log('listening on' + port);
 
+mongoose.connect(config.dbURL,function(error){
+    if(error){
+        console.log('Attempted DB Connection has failed');
+    }
+     else console.log ('Successfully connected to Database');   
+});
