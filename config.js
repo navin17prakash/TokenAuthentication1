50011168598f6feb57a87aefdd640f8f845b11cb
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-01-17 20:32:49 +0530

    Basic Functionality achieved

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var dbURL= 'mongodb://navinprakash:123456@ds037415.mongolab.com:37415/mnotes';
 var secret = 'radiantnavin';
+var modelName = 'userDataBank';
 
 module.exports.secret = secret;
 module.exports.dbURL =dbURL;
+module.exports.modelName = modelName;
 
--- server.js
+++ server.js
@@ -35,11 +35,11 @@ console.log('listening on' + port);
 console.log('value of supersecret is' + app.get('superSecret'));
 
 
-app.get('/setup',function(request,response){
+apiRoutes.get('/setuprootuser',function(request,response){
     var firstUser= new User({
-        name : 'navin',
+        name : 'navinprakash',
         password : 'indiaindia',
-        admin : false
+        admin : true
     });
     firstUser.save(function(error){
         if(! error)
@@ -63,12 +63,10 @@ apiRoutes.get('/users',function(request,response){
     
 });
 
-apiRoutes.post('/createuser',function(request,response){
-    console.log('post api is hit ');
+apiRoutes.get('/allusers',function(request,response){
+    console.log('Getting all the users');
     console.log(request.body);
     console.log('value sent is' + request.body.navin);
-    //console.log(request.body.navin1);
-    
 });
 
 apiRoutes.post( '/login',function(request,response) {
@@ -86,7 +84,7 @@ apiRoutes.post( '/login',function(request,response) {
             {
                 isLoginSuccess =true;
                 console.log('login performed by'+ request.body.name);
-                var token =jwt.sign(user,app.get('superSecret'),{expiresInMinutes : 120});
+                var token =jwt.sign(user,app.get('superSecret'),{expiresIn : 86400});
                 var responseJSON ={
                     name : request.body.name,
                     token : token,
@@ -97,6 +95,75 @@ apiRoutes.post( '/login',function(request,response) {
         }
         })
 });
+apiRoutes.post('/createuser',function createUserCallback(request,response){
+    var isTokenVerfied=false;
+    var responseJSON;
+    var token = request.body.token || request.query.token || request.headers['x-access-token'];
+    if(token){
+        //decode and verify token
+        jwt.verify(token, app.get('superSecret'), function(error,decoded){
+            //if verificition is successful then
+            console.log('value of error is' + error);
+            if(error){
+                console.log('Failed to decode the Token');
+                responseJSON = {
+                    success : false,
+                    message : 'Wrong or empty token has been provided'
+                };//close responseJSON
+                isTokenVerfied = false;
+                response.json(responseJSON);
+                
+            }//emd inner if block 
+            else {
+            isTokenVerfied =true;
+            //create user if token has been provided
+            responseJSON = gappu(isTokenVerfied,request);
+            response.json(responseJSON);
+            }//end inner else block 
+        });
+    }
+    
+    
+});
+var gappu = function(isTokenVerified,request){
+    console.log('am called');
+    var adminJSON;
+    var responseJSON;
+    console.log('creating user by navin');
+    User.findOne({name : request.body.name},function(error,user){
+        if(error) { adminJSON = { message : 'Credential entered is wrong'};
+                    
+                  }
+        else {
+            if(user.admin){
+                console.log('admin user has logged');
+                adminJSON = {message : 'Admin found,User created'};
+                console.log('trying hard');
+                var createdUser = new User({
+                                    name : request.body.newName,
+                                    password : request.body.newPassword,
+                                    admin : request.body.newAdmin
+    });
+                createdUser.save(function(error){
+                             if(!error)
+                                 {
+                                    console.log('1 user has been saved successfully');
+                                    responseJSON = { 
+                    admin : adminJSON
+    };
+    
+    
+                                   }
+                                else console.log('save has failed');
+                              });
+            } //end admin checking
+         else { console.log('Not logged as an admin');}   
+        } //end findone else
+        
+    });
+    return responseJSON;
+    }
+
 
 app.use('/api',apiRoutes);  
 
