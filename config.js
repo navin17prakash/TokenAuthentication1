d29f81fa56f7f56e59a239b5fb249b468db32840
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-01-18 01:12:49 +0530

    updated config.js

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
+//database connection URL
 var dbURL= 'mongodb://navinprakash:123456@ds037415.mongolab.com:37415/mnotes';
+//Key for hashing the JSON web tokens
 var secret = 'radiantnavin';
+//name of the model against which the collections will be created on the 
 var modelName = 'userDataBank';
 
 module.exports.secret = secret;
